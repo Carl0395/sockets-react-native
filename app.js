0c94a1438bc66f0ce31b8b6539b5679eeb1535b9
Carlos <arturo0395@gmail.com>
2017-08-07 19:13:28 -0500

    Inicio del proyecto

---
+++ app.js
@@ -0,0 +1,67 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import SocketIoClient from 'socket.io-client';
+export default class Sockets extends Component {
+
+  constructor(props){
+    super(props);
+    this.onReceive = this.receiveMessage.bind(this);
+    this.socket = SocketIoClient('http://localhost:8080');
+    this.socket.on('Messages', this.onReceive);
+  }
+
+  /** Cuando el servidor envia un mensaje
+   *
+   */
+  receiveMessage = (message) => {
+    console.warn("nuevo", message)
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('Sockets', () => Sockets);
