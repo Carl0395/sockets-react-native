3456fd404df6f5f0e8c65ccace3a9d414683959a
User <user@iMac-de-User.local>
2017-09-21 14:15:48 -0500

    init design

--- app.js
+++ app.js
@@ -4,64 +4,122 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    FlatList,
+    TouchableOpacity,
+    TextInput
 } from 'react-native';
 import SocketIoClient from 'socket.io-client';
 export default class Sockets extends Component {
 
-  constructor(props){
-    super(props);
-    this.onReceive = this.receiveMessage.bind(this);
-    this.socket = SocketIoClient('http://localhost:8080');
-    this.socket.on('Messages', this.onReceive);
-  }
+    constructor(props) {
+        super(props);
 
-  /** Cuando el servidor envia un mensaje
-   *
-   */
-  receiveMessage = (message) => {
-    console.warn("nuevo", message)
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+
+        this.state = {
+            dataSource: ['valor'],
+            text: ''
+        };
+
+        this.onReceive = this.receiveMessage.bind(this);
+        this.socket = SocketIoClient('https://chat-react-native.herokuapp.com');
+        this.socket.on('message', this.onReceive);
+    }
+
+    componentDidMount() {
+
+    }
+
+    /** Cuando el servidor envia un mensaje
+     *
+     */
+    receiveMessage = (message) => {
+        console.warn("nuevo", message);
+        let data = this.state.dataSource;
+        data.push(message);
+        this.setState({
+            dataSource: data,
+        })
+    };
+
+    onSend(messages = []) {
+        this.socket.emit('message', {name: 'celular', text: 'prueba movil'});
+    }
+
+
+
+    _renderItem = (item) => {
+        console.warn(item.item)
+        return (
+            <View style={{flex: 1}}>
+                <Text style={{backgroundColor: 'red', top: 10}}>{item.item}</Text>
+            </View>
+        )
+    };
+
+    addItem = (item) => {
+
+        let object = item;
+        this.state.dataSource.push(object);
+        this.setState({dataSource: this.state.dataSource})
+        console.warn(item, this.state.dataSource)
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+
+                <FlatList
+                    style={{top: 0, marginBottom: 4, right: 0, bottom: 40, marginTop: 20}}
+                    enableEmptySections = {true}
+                    data={this.state.dataSource}
+                    initialNumToRender={27}
+                    scrollEnabled={false}
+                    showsVerticalScrollIndicator={false}
+                    keyExtractor={item => item}
+                    renderItem={(item) => {
+                        return (
+                            <View>
+                                <View style={{alignItems: 'center'}}>
+                                    <Text style={{flex: 1, fontWeight: 'bold', maxHeight: 15}}>{item.item}</Text>
+                                </View>
+                            </View>
+                        )
+                    }}
+                />
+
+                <View style={{position: 'absolute', left: 0, right: 0, bottom: 0, height: 50, backgroundColor: '#FFF', borderTopWidth: 1, borderTopColor: '#ccc'}}>
+                    <TextInput style={{height: 50, marginLeft: 10}}
+                            placeholder={"Escriba un mensaje"}/>
+                    <TouchableOpacity style={{position: 'absolute',}}>
+                        <Text>Button</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#eaffff'
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('Sockets', () => Sockets);
